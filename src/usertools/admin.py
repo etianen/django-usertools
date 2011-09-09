eac2c227685ff19fb9578cf3438d037fcc15512d
Dave Hall <dave@etianen.com>
2011-09-09 19:04:11 +0100

    Added user count to group admin.

--- src/usertools/admin.py
+++ src/usertools/admin.py
@@ -1,9 +1,10 @@
 """Admin classes for django-usertools."""
 
 from django.conf import settings
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as UserAdminBase
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.admin import UserAdmin as UserAdminBase, GroupAdmin as GroupAdminBase
 from django.contrib import admin
+from django.db.models import Sum
 
 
 # Mix in watson search, if available.
@@ -28,3 +29,28 @@ class UserAdmin(UserAdminBase, AdminBase):
 # Automatcally re-register the User model with the enhanced admin class.    
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+
+class GroupAdmin(GroupAdminBase, AdminBase):
+
+    """Enhanced group admin class."""
+    
+    list_display= ("name", "get_user_count",)
+    
+    def queryset(self, request):
+        """Modifies the queryset."""
+        qs = super(GroupAdmin, self).queryset(request)
+        qs = qs.annotate(
+            user_count = Sum("user"),
+        )
+        return qs
+        
+    def get_user_count(self, obj):
+        """Returns the number of users in the given group."""
+        return obj.user_count or 0
+    get_user_count.short_description = "members"
+    
+    
+# Automatcally re-register the Group model with the enhanced admin class.    
+admin.site.unregister(Group)
+admin.site.register(Group, GroupAdmin)
