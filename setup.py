32515c173eb68996f9f47c7ddae57bf34a1a2199
Dave Hall <dave@etianen.com>
2015-12-02 15:02:24 +0000

    Bumping version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name = "django-usertools",
-    version = "1.0.10",
+    version = "1.0.11",
     description = "A fire-and-forget enhancement to the Django user admin.",
     author = "Dave Hall",
     author_email = "dave@etianen.com",
