bf60e3c348e7da113b1e2071bb9e96728f044f08
Dave Hall <dave@etianen.com>
2015-12-02 15:07:42 +0000

    Fixing strings in url() calls

--- src/usertools/urls.py
+++ src/usertools/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.core.urlresolvers import reverse_lazy
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
     # Password reset workflow.
-    url("^password-reset/$", "django.contrib.auth.views.password_reset", name="admin_password_reset", kwargs={
+    url("^password-reset/$", auth_views.password_reset, name="admin_password_reset", kwargs={
         "email_template_name": "admin/auth/user/password_reset_email.txt",
     }),
-    url("^password-reset/complete/$", "django.contrib.auth.views.password_reset_done", name="password_reset_done"),
-    url("^password-reset/token/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$", "django.contrib.auth.views.password_reset_confirm", name="password_reset_confirm"),
-    url("^password-reset/token/complete/$", "django.contrib.auth.views.password_reset_complete", name="password_reset_complete", kwargs={
+    url("^password-reset/complete/$", auth_views.password_reset_done, name="password_reset_done"),
+    url("^password-reset/token/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$", auth_views.password_reset_confirm, name="password_reset_confirm"),
+    url("^password-reset/token/complete/$", auth_views.password_reset_complete, name="password_reset_complete", kwargs={
         "extra_context": {
             "login_url": reverse_lazy("{app_name}:index".format(app_name=admin.site.name)),
         },
