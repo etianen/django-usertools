23de4546bf67d59546eaa8f8e2851dcd0a433730
Dave Hall <dave@etianen.com>
2015-12-02 15:00:00 +0000

    Tweaking settings

--- src/test_project/test_project/settings.py
+++ src/test_project/test_project/settings.py
@@ -103,12 +103,12 @@ TEMPLATES = (
         "OPTIONS": {
             "context_processors": (
                 "django.contrib.auth.context_processors.auth",
-                "django.core.context_processors.debug",
-                "django.core.context_processors.i18n",
-                "django.core.context_processors.media",
-                "django.core.context_processors.static",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
                 "django.contrib.messages.context_processors.messages",
-                "django.core.context_processors.request",
+                "django.template.context_processors.request",
             ),
             "loaders": (
                 'django.template.loaders.filesystem.Loader',
