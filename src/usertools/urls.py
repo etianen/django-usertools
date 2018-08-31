1d8364b818a8e7023fd0077ec89bde0b012e430f
fabiopintodacosta <6498561+fabiopintodacosta@users.noreply.github.com>
2018-08-31 14:17:37 +0100

    Still failed in the styleguides

--- src/usertools/urls.py
+++ src/usertools/urls.py
@@ -9,18 +9,32 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # Password reset workflow.
-    url("^password-reset/$", auth_views.PasswordResetView.as_view(), name="admin_password_reset", kwargs={
-        "email_template_name": "admin/auth/user/password_reset_email.txt",
-    }),
-    url("^password-reset/complete/$", auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    url(
+        "^password-reset/$",
+        auth_views.PasswordResetView.as_view(),
+        name="admin_password_reset",
+        kwargs={
+            "email_template_name": "admin/auth/user/password_reset_email.txt",
+        }
+    ),
+    url(
+        "^password-reset/complete/$",
+        auth_views.PasswordResetDoneView.as_view(),
+        name="password_reset_done"
+    ),
     url(
         "^password-reset/token/(?P<uidb64>[0-9A-Za-z]+)-(?P<token>.+)/$",
         auth_views.PasswordResetConfirmView.as_view(),
         name="password_reset_confirm",
     ),
-    url("^password-reset/token/complete/$", auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete", kwargs={
-        "extra_context": {
-            "login_url": reverse_lazy("{app_name}:index".format(app_name=admin.site.name)),
-        },
-    }),
+    url(
+        "^password-reset/token/complete/$",
+        auth_views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+        kwargs={
+            "extra_context": {
+                "login_url": reverse_lazy("{app_name}:index".format(app_name=admin.site.name)),
+            },
+        }
+    ),
 ]
